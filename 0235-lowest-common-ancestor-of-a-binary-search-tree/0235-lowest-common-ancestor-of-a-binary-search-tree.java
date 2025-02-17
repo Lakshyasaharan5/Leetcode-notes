8befeb39d7de43ef36b44732156f7d0ea084f9a7
Jason_statham <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-02-17 01:28:37 -0500

    Time: 5 ms (100%), Space: 45.3 MB (36.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,23 +1,23 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root != null){
-            if((root.val == p.val || root.val == q.val) || (root.val > p.val && root.val < q.val) || (root.val < p.val && root.val > q.val))
-                return root;
-            
-            if(root.val > p.val && root.val > q.val)
-                return lowestCommonAncestor(root.left, p, q);
-            
-            return lowestCommonAncestor(root.right, p, q);
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left; // Both nodes are in the left subtree
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right; // Both nodes are in the right subtree
+            } else {
+                return root; // Found LCA (either root is p/q or lies between p and q)
+            }
         }
         return null;
     }
