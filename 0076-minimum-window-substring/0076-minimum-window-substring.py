fa7afee7d08729bd19de1b43622b8ad74d398e2d
Jason_statham <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-04-27 22:56:24 -0400

    Time: 50 ms (93.87%), Space: 18.4 MB (35.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,43 +1,25 @@
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        """
-        ADOBECODEBANC
-                 i
-                    j
-
-        A : 0
-        B : 1
-        C : 0
-        allDone = 1
-        """
-        if len(s) < len(t):
-            return ""
-        find = {}
-        for i in t:
-            if i in find:
-                find[i] += 1
-            else:
-                find[i] = 1
-
-        allDone = len(set(t))
-        result = []
-        minWindow = float("inf")
-        i, j = 0, 0
-        while j < len(s):
-            if s[j] in find:
-                find[s[j]] -= 1
-                if find[s[j]] == 0:
-                    allDone -= 1
-                while allDone == 0 and i <= j:
-                    if j - i + 1 < minWindow:
-                        result = [i, j]
-                        minWindow = j - i + 1
-                    if s[i] in find:
-                        find[s[i]] += 1
-                        if find[s[i]] > 0:
-                            allDone += 1
-                    i += 1
-            j += 1
-
-        return s[result[0]: result[1] + 1] if len(result) > 0 else ""
-
+    def minWindow(self, s: str, t: str) -> str: 
+        if len(t) > len(s): return ""      
+        freq = [0] * 256
+        for ch in t:
+            freq[ord(ch)] += 1
+        cnt = len(t)
+        left, right = 0, 0
+        minLen, idx = float("inf"), 0
+        while right < len(s):
+            if freq[ord(s[right])] > 0:
+                cnt -= 1
+            freq[ord(s[right])] -= 1            
+            while cnt == 0:
+                if right - left + 1 < minLen:
+                    minLen = right - left + 1
+                    idx = left
+                freq[ord(s[left])] += 1
+                if(freq[ord(s[left])] > 0):
+                    cnt += 1
+                left += 1
+            right += 1
+        return s[idx : idx+minLen] if minLen < float('inf') else ""
+            
+        
