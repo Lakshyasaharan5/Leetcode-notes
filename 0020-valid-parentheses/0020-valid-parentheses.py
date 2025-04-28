2074dbf356241a6a46fd945b8737e36f41b5ae2b
Jason_statham <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-04-27 23:10:46 -0400

    Time: 0 ms (100%), Space: 17.7 MB (96.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        map = {'(':')', '[':']', '{':'}'}
         stack = []
-
-        # Mapping of closing parentheses to their corresponding opening parentheses
-        matching_brackets = {")": "(", "]": "[", "}": "{"}
-
-        for paran in s:
-            # If the character is a closing parenthesis
-            if paran in matching_brackets:
-                # Check if the stack is empty or the top of the stack doesn't match
-                if len(stack) == 0 or stack[-1] != matching_brackets[paran]:
-                    return False
-                stack.pop()  # Remove the last element in the stack (this was a valid match)
+        for p in s:
+            if p in map:
+                stack.append(p)
             else:
-                # If it's an opening parenthesis, push it onto the stack
-                stack.append(paran)
-
-        # If stack is empty, all parentheses are matched
-        if len(stack) == 0:
-            return True
-        else:
-            return False
+                if len(stack) == 0 or map[stack.pop()] != p:
+                    return False
+        return True if len(stack) == 0 else False
