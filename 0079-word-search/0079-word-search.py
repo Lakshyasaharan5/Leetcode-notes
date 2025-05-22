46598aa32bb25058525ea78e1444829fc136e155
Jason_statham <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-05-21 23:09:47 -0400

    Time: 3447 ms (74.97%), Space: 17.8 MB (87.4%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,33 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:        
-        def dfs(board, i, j, word, itr):
-            if itr == len(word):
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        """
+        mark
+        dfs()
+        unmark
+        """
+
+        def dfs(board, word, i, j, idx):
+            if idx == len(word):
                 return True
-            if i == len(board) or j == len(board[0]) or i < 0 or j < 0:
+            if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or board[i][j]!=word[idx]: 
                 return False            
-            if board[i][j] != word[itr]:
-                return False
-            board[i][j]='*'
-            res = dfs(board, i+1, j, word, itr+1) or dfs(board, i-1, j, word, itr+1) or dfs(board, i, j+1, word, itr+1) or dfs(board, i, j-1, word, itr+1)
-            board[i][j]=word[itr]
-            return res
+            save = board[i][j] #save current char before marking
+            board[i][j] = '*'
+            found = (
+                    dfs(board, word, i+1, j, idx+1) or 
+                    dfs(board, word, i, j+1, idx+1) or 
+                    dfs(board, word, i-1, j, idx+1) or 
+                    dfs(board, word, i, j-1, idx+1)
+            )
+            board[i][j] = save #unmark
+            return found
+
+        # Iterating over board to find first matching char of string
         for i in range(len(board)):
             for j in range(len(board[0])):
+                # Start dfs
                 if board[i][j] == word[0]:
-                    if dfs(board, i, j, word, 0):
+                    if(dfs(board, word, i, j, 0)):
                         return True
-
+        
         return False
-            
