7401566c86f9efc9f9e664bbd40e7961e5b3be52
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-06-13 03:23:59 -0400

    Time: 4 ms (74.66%), Space: 44.6 MB (51.03%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,23 +10,21 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
-        if(lists.length == 1) return lists[0];
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((ListNode a, ListNode b) -> a.val - b.val);
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        ListNode dummy = new ListNode(-1);
+        ListNode res = dummy;
         for(ListNode node : lists){
-            if(node!=null)
+            if(node != null)
                 pq.add(node);
-        }   
-        if(pq.size() == 0) return null;
-        ListNode tmp = pq.poll();
-        ListNode head = tmp;
-        if(tmp!=null && tmp.next!=null) pq.add(tmp.next);    
-        while(pq.size() > 0){
-            ListNode top = pq.poll();
-            tmp.next = top;
-            if(top!=null && top.next!=null) pq.add(top.next);    
-            tmp = tmp.next;
         }
-        return head;
+        while(pq.size() > 0){
+            ListNode node = pq.poll();
+            dummy.next = node;
+            dummy = dummy.next;
+            if(node.next != null){
+                pq.add(node.next);
+            }
+        }
+        return res.next;
     }
 }
