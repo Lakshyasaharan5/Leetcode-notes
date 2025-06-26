e601783cbc859d8e612333d73bc9255eb60def04
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-06-25 22:27:01 -0400

    Time: 23 ms (46.83%), Space: 17.9 MB (35.04%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,20 +1,34 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
+        res, tmp = [], []
+        candidates.sort()
 
-        def dfs(candidates, target, subset, i):
-            if target <= 0:
-                if target == 0:
-                    res.append(subset.copy())
+        def backtrack(candidates, target, idx, curr_sum):
+            if curr_sum == target:
+                res.append(copy.deepcopy(tmp))
                 return
-            prev = 0
-            for j in range(i, len(candidates)):
-                if prev == candidates[j]:
-                    continue                
-                subset.append(candidates[j])
-                dfs(candidates, target-candidates[j], subset, j+1)
-                subset.pop()
-                prev = candidates[j]
-                
-        dfs(sorted(candidates), target, [], 0)
-        return res
+            
+            if curr_sum > target or idx == len(candidates):
+                return
+
+            for i in range(idx, len(candidates)):
+                if(i>idx and candidates[i] == candidates[i-1]):
+                    continue
+                tmp.append(candidates[i])
+                backtrack(candidates, target, i+1, curr_sum+candidates[i])
+                tmp.pop()
+
+        backtrack(candidates, target, 0, 0)
+        
+        return res 
+
+
+
+
+
+
+
+
+
+
+
