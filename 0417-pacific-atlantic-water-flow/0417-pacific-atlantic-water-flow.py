55258c5e319f0c74b6786c1d86e9be0aa7e8a024
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-07-14 09:48:38 -0400

    Time: 27 ms (84.43%), Space: 19.6 MB (29.75%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,34 +1,11 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows, cols = len(heights), len(heights[0])
-        visited = [[False for _ in range(cols)] for _ in range(rows)]
-        pacific_visited = [[False for _ in range(cols)] for _ in range(rows)]
+        visited = [[False for _ in range(cols)] for _ in range(rows)]        
         directions = [(1, 0), (0, 1), (0, -1), (-1, 0)]
         res = []
 
         def bfs(r, c):
-            q = deque([(r, c)])
-            visited[r][c] = True
-            pacific_visited[r][c] = True
-
-            while q:
-                cr, cc = q.popleft()
-
-                for dr, dc in directions:
-                    nr, nc = cr + dr, cc + dc
-                    if (
-                        0 <= nr < rows
-                        and 0 <= nc < cols
-                        and not visited[nr][nc]
-                        and heights[nr][nc] >= heights[cr][cc]
-                    ):
-                        visited[nr][nc] = True
-                        q.append((nr, nc))
-                        pacific_visited[nr][nc] = True
-
-            return False
-
-        def bfs2(r, c):
             q = deque([(r, c)])
             visited[r][c] = True
 
@@ -50,6 +27,7 @@ class Solution:
             return False
 
         # check pacific side
+        pacific_visited = [[False for _ in range(cols)] for _ in range(rows)]
         for c in range(0, cols):
             if not visited[0][c]:
                 bfs(0, c)
@@ -58,14 +36,15 @@ class Solution:
             if not visited[r][0]:
                 bfs(r, 0)
 
+        pacific_visited = [row[:] for row in visited]
         visited = [[False for _ in range(cols)] for _ in range(rows)]
         # check atlantic side
         for c in range(0, cols):
             if not visited[rows - 1][c]:
-                bfs2(rows - 1, c)
+                bfs(rows - 1, c)
 
         for r in range(0, rows):
             if not visited[r][cols - 1]:
-                bfs2(r, cols - 1)
+                bfs(r, cols - 1)
 
         return res
