7faa9b2690e11ab04a2fe058596dc2f9b04c0835
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-07-20 16:46:04 -0400

    Time: 0 ms (100%), Space: 17.7 MB (78.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,14 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
+    def climbStairs(self, n: int) -> int:        
+        if (n <= 3):
+            return n
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
 
-        def dfs(n):
-            if n <= 3:
-                return n
-            if dp[n] != 0:
-                return dp[n]
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
 
-            dp[n] = dfs(n - 1) + dfs(n - 2)
-            return dp[n]
+        return dp[n]
 
-        return dfs(n)
+        
