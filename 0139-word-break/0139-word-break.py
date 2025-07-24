e691736ca0e3980c9aadc8dd588b2bb7277e638a
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-07-24 12:44:24 -0400

    Time: 3 ms (70.68%), Space: 17.8 MB (63.81%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -5,7 +5,7 @@ class Solution:
         wordSet = set(wordDict)
         for i in range(len(s) - 1, -1, -1):
             for j in range(i, len(s)):
-                if s[i : j+1] in wordSet:
-                    if dp[j + 1]:
-                        dp[i] = True
+                if s[i : j+1] in wordSet and dp[j + 1]:
+                    dp[i] = True
+                    break
         return dp[0]
