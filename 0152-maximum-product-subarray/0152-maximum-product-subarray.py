41281d5d86995cf8fadc9f7db465ce5cab7ca3ed
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-07-23 21:26:49 -0400

    Time: 14 ms (13.16%), Space: 18.5 MB (14.98%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,7 +3,6 @@ class Solution:
         res = nums[0]
         best, worse = nums[0], nums[0]
         prev_best, prev_worse = best, worse
-        print(best, worse)
         for i in range(1, len(nums)):
             prev_best, prev_worse = best, worse
             if nums[i-1] == 0:
@@ -12,5 +11,4 @@ class Solution:
                 best = max(nums[i], nums[i] * prev_best, nums[i] * prev_worse)
                 worse = min(nums[i], nums[i] * prev_best, nums[i] * prev_worse)
             res = max(res, best)
-            print(best, worse)
         return res
