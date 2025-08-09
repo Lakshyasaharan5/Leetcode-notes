20b860817252cd4b416d16b1701113e79f5fae77
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-08-09 19:58:50 -0400

    Time: 3 ms (96.89%), Space: 18.6 MB (75.71%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,17 +1,28 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = {}
-        def dfs(start):  
-            if start in dp:
-                return dp[start]          
-            if start >= len(nums) - 1:
-                return 0
-            if nums[start] == 0:
-                return float('inf')
-            jumps = float('inf')
-            for i in range(1, nums[start] + 1):                
-                jumps = min(jumps, dfs(start + i))
-            dp[start] = jumps + 1
-            return jumps + 1
+        """
+        0 1 2 3 4
+        1,2,3
+            ^
+            curr_end = 3            
+            max_reach = 3  
+            
+        jumps = 1
+        """
+        if len(nums) == 1:
+            return 0
+        curr_end = nums[0]
+        max_reach = nums[0]
+        jumps = 1
+        for i in range(len(nums) - 1):
+            if i + nums[i] > max_reach:
+                max_reach = i + nums[i]
+            if i == curr_end:
+                curr_end = max_reach
+                jumps += 1
 
-        return dfs(0) 
+        return jumps
+
+
+        
+        
