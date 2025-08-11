05bca928161bb835f68d0bac871c03f8ee1e2ddb
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-08-11 13:33:34 -0700

    Time: 7 ms (97.83%), Space: 22.7 MB (87.17%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.py
+++ 1428-jump-game-iii/1428-jump-game-iii.py
@@ -1,6 +1,16 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        if start < 0 or start >= len(arr) or arr[start] < 0: return False
-        arr[start] = -arr[start]
-        return arr[start] == 0 or self.canReach(arr, start - arr[start]) or self.canReach(arr, start + arr[start])
-        
+        queue = deque([start])
+        while queue:
+            node = queue.popleft()
+            if arr[node] == 0: return True
+            left = node - arr[node]
+            right = node + arr[node]
+            if left >= 0 and left < len(arr) and arr[left] >= 0:
+                arr[left] = -arr[left]
+                queue.append(left)
+            if right >= 0 and right < len(arr) and arr[right] >= 0:
+                arr[right] = -arr[right]
+                queue.append(right)
+
+        return False
