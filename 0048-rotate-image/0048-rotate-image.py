e0c103cb15da2350a1aa638c97bd6f4b6057e0f1
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-08-16 02:24:17 -0400

    Time: 0 ms (100%), Space: 17.8 MB (66.7%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # Swap along the row from top and bottom moving towards each other
+        top, bottom = 0, len(matrix) - 1
+        while top < bottom:
+            matrix[top], matrix[bottom] = matrix[bottom], matrix[top]
+            top += 1
+            bottom -= 1
+        print(matrix)
+
+        # Fix diagonal and swap elements on opposite sides with each other
+        idx = 1
+        while idx < len(matrix):
+            i, j = idx, 0
+            while i < len(matrix):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                i += 1
+                j += 1
+            idx += 1
+            # 00 01 02 03
+            # 10 11 12 13
+            # 20 21 22 23
+            # 30 31 32 33
