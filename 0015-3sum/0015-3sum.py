d63c9ba6e2dd809ae6b3d14ba0fe6ee7830005a7
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-09-09 21:21:04 -0400

    Time: 847 ms (19.95%), Space: 20.7 MB (70.15%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # -1 -1 0 1 2 4
+        #       l
+        #       r
+        # 0 1 2
         nums.sort()
+        n = len(nums)
         res = []
-        for i in range(len(nums) - 2):
+        for i in range(n - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l, h = i + 1, len(nums) - 1
-            complement = -1 * nums[i]
-            while l < h:
-                if nums[l] + nums[h] == complement:
-                    res.append([nums[i], nums[l], nums[h]])
-                    l += 1
-                    h -= 1
-                    while l < h and nums[l] == nums[l - 1]:
-                        l += 1
-                    while l < h and nums[h] == nums[h + 1]:
-                        h -= 1
-                elif nums[l] + nums[h] > complement:
-                    h -= 1
+            find = -nums[i]
+            l, r = i + 1, n - 1
+            while l < r:
+                if nums[l] + nums[r] == find:
+                    res.append([nums[i], nums[l], nums[r]])
+                    
+                if nums[l] + nums[r] > find:
+                    r -= 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
                 else:
                     l += 1
-        
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
         return res
