e548492a761edd65fd33d273512c314175441228
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-09-15 23:16:53 -0400

    Time: 0 ms (100%), Space: 46.8 MB (14.1%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,34 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int result = 0;
-        int startIndex = 0;        
-
-        //we want monotonic decreasing stack so skip to the point where height start to decline
-        while(startIndex < height.length - 1 && height[startIndex] <= height[startIndex + 1] ){
-            startIndex++;
-        }
-
-        Stack<Integer> stack = new Stack<>();
-        stack.push(startIndex);
-        
-        for(int i = startIndex + 1; i < height.length; i++){
-            //in case of duplicate just pop when they come like if we have 1 1 1 then each 1 will remove the previous 1 from stack
-            if(height[i] < height[stack.peek()]){
-                stack.push(i);
+        int l = 0, r = height.length - 1;
+        int water = 0;
+        int leftMax = 0, rightMax = 0;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (leftMax > height[l]) {
+                    water += leftMax - height[l];
+                }
+                leftMax = Math.max(leftMax, height[l]);
+                l++;
             }else{
-                int hinderanceBetween = height[stack.peek()];
-                while(!stack.empty() && height[stack.peek()] <= height[i]){
-                    int top = stack.pop();
-                    result += (i - top - 1) * (Math.min(height[i], height[top]) - hinderanceBetween);
-                    hinderanceBetween = Math.max(height[top], hinderanceBetween);
+                if (rightMax > height[r]){
+                    water += rightMax - height[r];
                 }
-                if(!stack.empty()){
-                    result += (i - stack.peek() - 1) * (Math.min(height[i], height[stack.peek()]) - hinderanceBetween);
-                }
-                stack.push(i);
+                rightMax = Math.max(rightMax, height[r]);
+                r--;
             }
         }
-
-        return result;
+        return water;
     }
 }
