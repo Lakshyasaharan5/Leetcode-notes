75101e358707cba13a96ba503929190f04a19360
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-09-15 21:05:57 -0400

    Time: 40 ms (25.39%), Space: 51.4 MB (70.22%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        for(int n: nums)
-            System.out.print(n +" ");
-        int k=0, i=0, j=0;
         // -4 -1 -1 0 1 2
-        //          k  
-        //            i-j    
-        while(k<nums.length-2){            
-            int target = -1*nums[k];
-            i=k+1;
-            j=nums.length-1;
-            while(i<j){
-                int twoSum = nums[i] + nums[j];
-                if(twoSum == target){
-                    List<Integer> curr = new ArrayList<>();
-                    curr.add(nums[k]);
-                    curr.add(nums[i]);
-                    curr.add(nums[j]);
-                    result.add(curr);
-                    i++;
-                    while(i<nums.length && i<j && nums[i]==nums[i-1])
-                        i++;
-                    j--;
-                    while(j>0 && j>i && nums[j]==nums[j+1])
-                        j--;
-                }else if(twoSum > target){
-                    j--;                    
-                }else{
-                    i++;
+        //     l 
+        //            r
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int target = -1 * nums[i];
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));                    
                 }
+                if (sum > target){
+                    r--;
+                    while (l < r && nums[r] == nums[r + 1]) r--;                                        
+                }
+                else{
+                    l++;                
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                }
+               
             }
-            k++;
-            while(k<nums.length-2 && nums[k]==nums[k-1])
-                k++;
         }
-        return result;
+        return res;
     }
 }
