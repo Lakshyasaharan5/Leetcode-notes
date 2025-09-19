bb828e052d67adcb12409d454c3cf35df230d8d9
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-09-18 22:34:22 -0400

    Time: 3 ms (46.94%), Space: 42.1 MB (17.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,9 +1,11 @@
-class Solution {
-    public boolean isValid(String s) {
-        HashMap<Character, Character> bracketPairs = new HashMap<>();
+class Solution {    
+    static HashMap<Character, Character> bracketPairs = new HashMap<>();
+    static{
         bracketPairs.put(')', '(');
         bracketPairs.put(']', '[');
         bracketPairs.put('}', '{');
+    }
+    public boolean isValid(String s) {        
         Stack<Character> bracketStack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char bracket = s.charAt(i);
