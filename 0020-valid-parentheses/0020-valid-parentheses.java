892f899d5182846e7967bf740055fc33682f9927
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-09-18 22:51:52 -0400

    Time: 0 ms (100%), Space: 41.8 MB (59.19%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,25 @@
-class Solution {    
-    static HashMap<Character, Character> bracketPairs = new HashMap<>();
-    static{
-        bracketPairs.put(')', '(');
-        bracketPairs.put(']', '[');
-        bracketPairs.put('}', '{');
-    }
-    public boolean isValid(String s) {        
-        Deque<Character> bracketStack = new ArrayDeque<>();
-        for (int i = 0; i < s.length(); i++) {
-            char bracket = s.charAt(i);
-            if (bracketPairs.containsKey(bracket)) {
-                if (bracketStack.isEmpty() || bracketStack.pop() != bracketPairs.get(bracket)) {
-                    return false;
-                }
-            } else {
-                bracketStack.push(bracket);
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0) return false;
+        char[] ch=s.toCharArray();
+        char[] stack=new char[s.length()];
+        int top=0;
+        for(int i=0;i<ch.length;i++){
+            switch(ch[i]){
+                case '(','{','[':
+                    stack[top++]=ch[i];
+                    break;
+                case ')':
+                    if(top==0||stack[--top]!='(') return false;
+                    break;
+                case '}':
+                    if(top==0||stack[--top]!='{') return false;
+                    break;
+                case ']':
+                    if(top==0||stack[--top]!='[') return false;
+                    break;
             }
-        }                
-        return bracketStack.isEmpty();
+        }
+        return top==0;
     }
 }
