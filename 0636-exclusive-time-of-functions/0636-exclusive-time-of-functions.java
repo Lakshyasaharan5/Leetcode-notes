8db1c1c6b6630744f6ffa3d24e53952eb6598061
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-09-22 00:48:35 -0400

    Time: 7 ms (98.75%), Space: 44.6 MB (99.84%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -1,25 +1,33 @@
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
-        int[][] stack = new int[logs.size()][3];
+        int len = logs.size();
+        int[] idStack = new int[len];
+        int[] startStack = new int[len];
+        int[] childStack = new int[len];
         int top = -1;
+
         int[] result = new int[n];
 
-        for (String func : logs) {
-            String[] arr = func.split(":");
-            int id = Integer.parseInt(arr[0]);
-            int time = Integer.parseInt(arr[2]);
-            if (arr[1].equals("start")) {
-                stack[++top] = new int[] { id, time, 0 };
+        for (String log : logs) {
+            int first = log.indexOf(':');
+            int second = log.indexOf(':', first + 1);
+
+            int id = Integer.parseInt(log.substring(0, first));
+            String type = log.substring(first + 1, second);
+            int time = Integer.parseInt(log.substring(second + 1));
+
+            if (type.equals("start")) {
+                ++top;
+                idStack[top] = id;
+                startStack[top] = time;
+                childStack[top] = 0;
             } else {
-                int[] currFunc = stack[top--];
-                int timeSpent = time - currFunc[1] + 1;
-                result[id] += timeSpent + (currFunc[2] != -1 ? -currFunc[2] : 0);
-                if (top >= 0) {
-                    stack[top][2] += timeSpent;
-                }
+                int total = time - startStack[top] + 1;
+                result[idStack[top]] += total - childStack[top];
+                if (top > 0) childStack[top - 1] += total;
+                --top;
             }
         }
-
         return result;
     }
 }
