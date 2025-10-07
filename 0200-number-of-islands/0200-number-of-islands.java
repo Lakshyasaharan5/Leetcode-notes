19e11a5f46e7ee5761b96f2165d1eabc0e19ef4c
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-10-06 23:57:27 -0400

    Time: 4 ms (49.99%), Space: 49.1 MB (91.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,32 +1,30 @@
 class Solution {
-    int[][] DIRECTIONS = new int[][]{{1,0}, {0,-1}, {-1,0}, {0,1}};
+    int m,n;
     public int numIslands(char[][] grid) {
-        int islands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    bfs(grid, i, j);
-                    islands++;
+        m = grid.length;
+        n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int res=0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    res++;
+                    helper(i, j, grid, visited);
                 }
             }
         }
-        return islands;
+        return res;
     }
 
-    private void bfs(char[][] grid, int i, int j) {
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{i, j});
-        grid[i][j] = '0';
-        while (que.size() > 0) {
-            int[] node = que.remove();
-            for (int[] dir : DIRECTIONS) {
-                int r = node[0] + dir[0];
-                int c = node[1] + dir[1];
-                if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0')
-                    continue;
-                grid[r][c] = '0';
-                que.add(new int[]{r,c});
-            }
+    public void helper(int i, int j, char[][] grid, boolean[][] visited){
+        if(i==m || j==n || i<0 || j<0) return;
+
+        if(grid[i][j]=='1' && !visited[i][j]){
+            visited[i][j]=true;        
+            helper(i+1, j, grid, visited);
+            helper(i, j+1, grid, visited);
+            helper(i, j-1, grid, visited);
+            helper(i-1, j, grid, visited);
         }
     }
 }
