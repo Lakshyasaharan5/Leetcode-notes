6abda106b91e323a26be81f1615cdf191845e22b
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-10-08 17:14:51 -0400

    Time: 9 ms (43.27%), Space: 46.5 MB (73.98%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) {
-            return intervals;
-        }
-        Arrays.sort(intervals, (a, b) -> {
-            return (a[0] - b[0]);
-        });
-
-        List<int[]> tempResult = new ArrayList<>();
-        int start = intervals[0][0], end = intervals[0][1];
-        for (int i = 0; i < intervals.length; i++) {
-            if (intervals[i][0] <= end){
-                end = Math.max(end, intervals[i][1]);
-            }else{
-                tempResult.add(new int[]{start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+    //     [[1,3],[2,6],[8,10],[15,18]]
+    //                            ^
+    //  res=[[1,6], [8,10], [15,18]]
+    //                         ^
+    // -----------          
+    //     ------
+    //        ----
+        //TODO: sort based on end times
+        Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] curr = intervals[0];
+        for (int[] interval : intervals) {            
+            if (interval[0] <= curr[1]) {
+                curr[1] = Math.max(curr[1], interval[1]);
+            } else {
+                merged.add(curr);
+                curr = interval;
             }
         }
-
-        tempResult.add(new int[]{start, end});
-
-        int[][] result = new int[tempResult.size()][2];
-        for(int i=0; i<tempResult.size(); i++){
-            result[i][0] = tempResult.get(i)[0];
-            result[i][1] = tempResult.get(i)[1];
+        merged.add(curr);
+        int[][] result = new int[merged.size()][2];        
+        int itr = 0;
+        for (int[] interval : merged) {
+            result[itr++] = interval;
         }
-
         return result;
     }
 }
