f352d1942822852e3867113a76a6e6f93dc737e4
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-10-17 13:57:42 -0400

    Time: 14 ms (57.82%), Space: 49.6 MB (6.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // create a freq map
-        // push keys in min heap of size k based on values
-        // create result array from min heap
+        // {1:3, 2:1, 3:4}
+        // [1,3],[3,4]
         HashMap<Integer, Integer> freq = new HashMap<>();
         for (int n : nums) {
             freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()){
-            minHeap.add(entry);
-            if (minHeap.size() > k){
-                minHeap.poll();
-            }
-        }
-        int[] result = new int[k];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        freq.forEach((key,val) -> {
+            pq.add(new int[]{key,val});
+            if (pq.size() > k) pq.poll();
+        });
+        int[] result = new int[pq.size()];
         int idx = 0;
-        while (!minHeap.isEmpty()){
-            result[idx++] = minHeap.poll().getKey();
+        while (!pq.isEmpty()) {
+            result[idx++] = pq.poll()[0];
         }
         return result;
     }
