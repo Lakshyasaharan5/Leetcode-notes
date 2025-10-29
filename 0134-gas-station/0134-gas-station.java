371d5540d1a45e87ffa6b23a7a23f305a8a39825
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-10-29 19:56:42 -0400

    Time: 1961 ms (6.63%), Space: 56.6 MB (20.87%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,21 +1,33 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // 2,3,1,9,9
-        // 3,3,3,0,0
-        //       0   
-        int curr = 0, totalCost = 0, totalGas = 0;
-        int start = 0;
-        for (int i = 0; i < gas.length; i++) {
-            totalCost += cost[i];
-            totalGas += gas[i];
-            if (curr + gas[i] - cost[i] >= 0) {
-                curr += gas[i] - cost[i];
-            } else {
-                start = i + 1;
-                curr = 0;
-            }
-        }
-        if (totalCost > totalGas) return -1;
-        return start;
+        if(gas.length==1 && gas[0]>=cost[0])return 0;
+        int index = 0;
+		boolean res = false;
+		while(index<gas.length) {
+			if(gas[index]>cost[index]) {
+				res = isPossible(index, gas, cost);
+				if(res)
+					break;
+			}
+			index++;
+		}
+        return res?index:-1;
     }
+    public boolean isPossible(int index, int[] gas, int[] cost) {
+		int temp = index;
+		int n=gas.length;
+		int left = gas[temp]-cost[temp]+gas[(temp+1)%n];
+		temp = (temp+1)%n; 
+		while(temp!=index) {
+			if(left<cost[temp])
+				return false;
+			left += gas[(temp+1)%n] - cost[temp];
+			temp = (temp+1)%n;
+			if(left<=0) {				
+				return false;
+			}
+		}		
+		return true;
+		
+	}
 }
