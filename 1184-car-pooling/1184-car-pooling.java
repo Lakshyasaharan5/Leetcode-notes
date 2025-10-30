24454b54acde86675b0ce08f590eb493719848ac
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-10-29 20:26:19 -0400

    Time: 6 ms (44.48%), Space: 45.1 MB (5.5%) - LeetHub

---
+++ 1184-car-pooling/1184-car-pooling.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips, (a,b) -> a[1] - b[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+        if (capacity < trips[0][0]) return false;
+        capacity -= trips[0][0];
+        pq.add(trips[0]);
+        for (int i = 1; i<trips.length; i++) {
+            while (!pq.isEmpty() && pq.peek()[2] <= trips[i][1]) {
+                capacity += pq.poll()[0];
+            }            
+            if (capacity < trips[i][0]) return false;
+            capacity -= trips[i][0];
+            pq.add(trips[i]);
+        }
+        return true;
+    }
+}
