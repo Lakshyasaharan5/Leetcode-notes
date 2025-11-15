bbcda5b116606bb411775c90b98f80256ae9ca8a
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-11-15 17:11:35 -0500

    Time: 3 ms (84.74%), Space: 51.7 MB (38.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,46 @@
 class Solution {
-    int m,n;
     public int numIslands(char[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int res=0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    res++;
-                    helper(i, j, grid, visited);
+        int m = grid.length, n = grid[0].length;
+
+        // create dsu parent array of the grid
+        int[] parents = new int[m * n];
+        for (int i = 0; i < m*n; i++) {
+            parents[i] = i;
+        }
+        int islands = 0;
+        // we traverse the grid checking down and right, updating dsu
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != '1') continue;
+                islands++;
+                // right
+                if (j+1 < n && grid[i][j+1] == '1') {
+                    islands = union(parents, i*n+j, i*n+j+1, islands);
+                }
+                // down
+                if (i+1 < m && grid[i+1][j] == '1') {
+                    islands = union(parents, i*n+j, (i+1)*n+j, islands);
                 }
             }
         }
-        return res;
+
+        return islands;
     }
 
-    public void helper(int i, int j, char[][] grid, boolean[][] visited){
-        if(i==m || j==n || i<0 || j<0) return;
-
-        if(grid[i][j]=='1' && !visited[i][j]){
-            visited[i][j]=true;        
-            helper(i+1, j, grid, visited);
-            helper(i, j+1, grid, visited);
-            helper(i, j-1, grid, visited);
-            helper(i-1, j, grid, visited);
+    private int find(int[] parents, int index) {
+        while (parents[index] != index) {
+            index = parents[index];
         }
+        return index;
+    }
+
+    private int union(int[] parents, int a, int b, int islands) {
+        int rootA = find(parents, a);
+        int rootB = find(parents, b);
+        if (rootA != rootB) {
+            islands--;
+            parents[rootB] = rootA;
+        }
+        return islands;
     }
 }
