da8ddf9e2a2dfe07888e7b2cdfeec5c7b9913fca
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-12-02 22:33:49 -0500

    Time: 1572 ms (5.03%), Space: 57.6 MB (33.98%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,31 @@
 class Solution {
-    Boolean[][] dp = new Boolean[201][20001];
+    Boolean[][] dp;
     public boolean canPartition(int[] nums) {
         int total = 0;
-        for (int n : nums) {
+        for (int n : nums)
             total += n;
-        }
-        if(total % 2 != 0) return false; // if total is odd, can't partition
-        return backtrack(nums, 0, total/2);
+        if (total % 2 != 0 || nums.length == 1)
+            return false;
+        // Arrays.sort(nums);
+        dp = new Boolean[nums.length + 1][total/2 + 1];
+        return dfs(nums, total / 2, 0);
     }
 
-    private boolean backtrack(int[] nums, int start, int target) {
-        if (target == 0) return true;
-        if (start == nums.length || target < 0) return false;
+    private boolean dfs(int[] nums, int target, int start) {
+        if (target == 0){
+            return true;
+        }
+        if (start >= nums.length)
+            return false;     
+        
         if (dp[start][target] != null) return dp[start][target];
         boolean curr = false;
         for (int i = start; i < nums.length; i++) {
-            curr |= backtrack(nums, i + 1, target - nums[i]);
+            if (target - nums[i] < 0)
+                continue;
+            curr |= dfs(nums, target - nums[i], i + 1);
         }
-        return dp[start][target] = curr;
+        dp[start][target] = curr;
+        return curr;
     }
 }
