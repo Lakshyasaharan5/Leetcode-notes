49ca38fe5d8a10faa9aaa5b831da0b687898dd2d
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2025-12-23 21:15:57 -0500

    Time: 0 ms (100%), Space: 43.7 MB (6.12%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,15 @@
 class Solution {
-    int[][] dp = new int[101][101];
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {        
-        for (int i = 0; i < 101; i++) {
-            for (int j = 0; j < 101; j++) {
-                dp[i][j] = -1;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[] dp = new int[n];
+        dp[n - 1] = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            if (obstacleGrid[i][n - 1] == 1) dp[n - 1] = 0;
+            for (int j = n - 2; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                else dp[j] += dp[j + 1];
             }
         }
-        return dfs(obstacleGrid, 0, 0, dp);
-    }
-
-    private int dfs(int[][] grid, int i, int j, int[][] dp) {
-        if (i < 0 || 
-            j < 0 || 
-            i >= grid.length || 
-            j >= grid[0].length || 
-            grid[i][j] == 1) return 0;
-        if (i == grid.length - 1 && j == grid[0].length - 1) return 1;
-        if (dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = dfs(grid, i + 1, j, dp) + dfs(grid, i, j + 1, dp);
+        return dp[0];
     }
 }
