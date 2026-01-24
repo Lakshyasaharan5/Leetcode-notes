fb24877926f7ba30c3d7ee39dc82752d15e8f32e
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-01-23 23:22:52 -0500

    Time: 3 ms (87.3%), Space: 52.5 MB (38.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,46 +1,23 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length, n = grid[0].length;
-
-        // create dsu parent array of the grid
-        int[] parents = new int[m * n];
-        for (int i = 0; i < m*n; i++) {
-            parents[i] = i;
-        }
         int islands = 0;
-        // we traverse the grid checking down and right, updating dsu
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] != '1') continue;
-                islands++;
-                // right
-                if (j+1 < n && grid[i][j+1] == '1') {
-                    islands = union(parents, i*n+j, i*n+j+1, islands);
-                }
-                // down
-                if (i+1 < m && grid[i+1][j] == '1') {
-                    islands = union(parents, i*n+j, (i+1)*n+j, islands);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    dfs(grid, i, j);
                 }
             }
         }
-
         return islands;
     }
 
-    private int find(int[] parents, int index) {
-        while (parents[index] != index) {
-            index = parents[index];
-        }
-        return index;
-    }
-
-    private int union(int[] parents, int a, int b, int islands) {
-        int rootA = find(parents, a);
-        int rootB = find(parents, b);
-        if (rootA != rootB) {
-            islands--;
-            parents[rootB] = rootA;
-        }
-        return islands;
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
     }
 }
