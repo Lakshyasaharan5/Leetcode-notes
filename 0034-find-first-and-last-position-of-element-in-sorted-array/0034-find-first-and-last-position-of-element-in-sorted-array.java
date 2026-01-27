4b41de144ea817476e99225e9c1bed4de241e67b
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-01-26 22:51:49 -0500

    Time: 0 ms (100%), Space: 48.4 MB (22.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        // 0 1 2 3 4 5
-        // 5,7,7,8,8,10
-        //       l 
-        //       m
-        //       r
-        if (nums.length == 0) return new int[]{-1, -1};
-        int l = 0, r = nums.length - 1;
-        // find start position by bring r and keeping even sum
-        while (l < r) {
-            int m = (l + r) / 2;
-            if (nums[m] < target) {
+        if (nums.length == 0)
+            return new int[] { -1, -1 };
+
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int m = (h - l) / 2 + l;
+            if (nums[m] >= target)
+                h = m;
+            else
                 l = m + 1;
-            } else {
-                r = m;
-            }
         }
-        if (nums[r] != target) return new int[]{-1, -1};
-        int[] res = new int[2];
-        res[0] = r;
-        // find end position by bringing l and keeping odd sum
-        l = 0; r = nums.length - 1;
-        while (l < r) {
-            int m = (l + r) / 2 + 1;
-            if (nums[m] > target) {
-                r = m - 1;
-            } else {
+
+        if (nums[l] != target)
+            return new int[] { -1, -1 };
+
+        int[] res = {l, -1};
+        l = 0; h = nums.length - 1;
+        while (l < h) {
+            int m = (h - l) / 2 + l + 1;
+            if (nums[m] <= target)
                 l = m;
-            }
+            else
+                h = m - 1;
         }
+
         res[1] = l;
         return res;
     }
