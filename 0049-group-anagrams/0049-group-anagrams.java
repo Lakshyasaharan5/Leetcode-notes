87d0a73aefb85a6ce42525cd99d8eca09e1fc7a4
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-01-30 08:17:58 -0500

    Time: 18 ms (27.2%), Space: 50.2 MB (25.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,33 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // iterate over strs
-        // for each string will encode into 26 char hash
-        // put that into hashmap
-        // iterate over hashmap to create final result
-        HashMap<String, List<String>> map = new HashMap<>();
+        HashMap<String, ArrayList<String>> sortedWordMap = new HashMap<>();
         for (String s : strs) {
-            String hash = encode(s);
-            if (!map.containsKey(hash)){
-                map.put(hash, new LinkedList<String>());
-            }            
-            map.get(hash).add(s);
+            int[] freq = new int[26];
+            for (char ch : s.toCharArray()) freq [ch - 'a']++;
+            StringBuilder sb = new StringBuilder();
+            for (int f : freq) sb.append(f).append('#');
+            String sorted = sb.toString();
+            sortedWordMap.putIfAbsent(sorted, new ArrayList<String>());
+            sortedWordMap.get(sorted).add(s);
         }
-        List<List<String>> result = new LinkedList<>();
-        for(List<String> list : map.values()){
-            result.add(list);
+        List<List<String>> res = new ArrayList<>();
+        for (ArrayList<String> group : sortedWordMap.values()) {
+            res.add(group);
         }
-        return result;
-    }
-
-    String encode(String s) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int f : freq){
-            sb.append(f).append("#");
-        }
-        return sb.toString();
+        return res;
     }
 }
