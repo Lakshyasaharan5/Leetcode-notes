bd9cf6547e5e5c98a96888a05b88fdc8d39a4f1a
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-01-31 14:57:09 -0500

    Time: 4 ms (52.33%), Space: 43.4 MB (25.05%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,7 +1,6 @@
 class Pair {
     char ch;
     int freq;
-
     Pair (char ch, int freq) {
         this.ch = ch;
         this.freq = freq;
@@ -10,27 +9,24 @@ class Pair {
 
 class Solution {
     public String reorganizeString(String s) {
-        int[] charFreq = new int[26];
-        for (char ch : s.toCharArray()){
-            charFreq[ch - 'a']++;
-            if (charFreq[ch - 'a'] > (s.length() + 1)/2) return "";
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
         }
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
-        for (int i = 0; i < 26; i++) {
-            if (charFreq[i] > 0) {
-                pq.offer(new Pair((char)('a' + i), charFreq[i]));
-            }
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            pq.offer(new Pair(entry.getKey(), entry.getValue()));
         }
-        Pair prev = new Pair('#', 0);
         StringBuilder sb = new StringBuilder();
+        Pair prev = null;
         while (!pq.isEmpty()) {
-            Pair curr = pq.poll();
+            Pair curr = pq.poll();            
             sb.append(curr.ch);
             curr.freq--;
-            if (prev.freq > 0) pq.offer(prev);
+            if (prev != null && prev.freq > 0) pq.offer(prev);
             prev = curr;
         }
         if (sb.length() < s.length()) return "";
-        return sb.toString();
+        return sb.toString();    
     }
 }
