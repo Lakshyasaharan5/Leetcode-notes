6ac90edb650cafbfab5b7e4e1302322819efc830
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-03-24 14:08:26 -0400

    Time: 122 ms (84.24%), Space: 32 MB (55.18%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.good = 0
+        def dfs(root, currMax):
+            if not root:
+                return
+            if root.val >= currMax:
+                self.good += 1
+                currMax = root.val
+            dfs(root.left, currMax)
+            dfs(root.right, currMax)
+        dfs(root, float('-inf'))
+        return self.good
