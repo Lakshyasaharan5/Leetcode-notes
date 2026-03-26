28071009c785ef95ffe0acb86031e47c12711da5
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-03-26 17:53:56 -0400

    Time: 0 ms (100%), Space: 19.3 MB (60.4%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,22 +1,16 @@
 class Solution:
-    def numDecodings(self, s: str) -> int: 
-        dp = [-1] * len(s)       
-        def dfs(s, i):
-            if i >= len(s):
-                return 1
-            if s[i] == "0":
-                return 0
-
-            if dp[i] >= 0:
-                return dp[i]
-
-            left = dfs(s, i+1)
-
-            right = 0
-            if i+1 < len(s) and 10 <= int(s[i:i+2]) <= 26:
-                right = dfs(s, i+2)
-
-            dp[i] = left + right
-            return dp[i]
-
-        return dfs(s, 0)
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        dp = [1] * (len(s) + 1)
+        if s[-1] == '0':
+            dp[len(s) - 1] = 0
+        for i in range(len(s) - 2, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+                continue
+            dp[i] = dp[i + 1]
+            if int(s[i:i+2]) <= 26:
+                print(int(s[i:i+2]))
+                dp[i] += dp[i + 2]
+        return dp[0]
