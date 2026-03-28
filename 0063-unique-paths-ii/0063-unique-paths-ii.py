77f7ec713dbf5c724a65c6fc47be6a084956a94e
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-03-28 14:59:34 -0400

    Time: 0 ms (100%), Space: 20.3 MB (6.91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,13 +3,14 @@ class Solution:
         ROWS = len(obstacleGrid)
         COLS = len(obstacleGrid[0])
         dp = {}
+        @cache
         def dfs(r, c):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS or obstacleGrid[r][c] == 1:
                 return 0
             if r == ROWS - 1 and c == COLS - 1:
                 return 1
-            if (r,c) in dp:
-                return dp[(r,c)]
+            # if (r,c) in dp:
+            #     return dp[(r,c)]
             dp[(r,c)] = dfs(r+1, c) + dfs(r, c+1)
             return dp[(r,c)]
         return dfs(0,0)
