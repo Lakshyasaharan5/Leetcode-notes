5ef8d8a71008638313c2b1b2daf9d39f2bca627d
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-03-28 17:11:16 -0400

    Time: 235 ms (81.84%), Space: 21.4 MB (95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == '0':
-                return
-            grid[r][c] = '0'
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-
+        ROWS, COLS = len(grid), len(grid[0])
         islands = 0
+        def bfs(r, c):
+            que = deque()
+            que.append((r,c))
+            grid[r][c] = '0'
+            while que:
+                (fr, fc) = que.popleft()
+                for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    nr = fr + dr
+                    nc = fc + dc
+                    if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == '0':
+                        continue
+                    grid[nr][nc] = '0'
+                    que.append((nr, nc))
+
         for i in range(ROWS):
             for j in range(COLS):
                 if grid[i][j] == '1':
+                    bfs(i, j)
                     islands += 1
-                    dfs(i, j)
-        
+
         return islands
+
