4c70ed88a310e9d3743d9eb22e0a3c4aa1671550
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-03-29 16:26:13 -0400

    Time: 0 ms (100%), Space: 19.9 MB (90.09%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,23 +1,18 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         M, N = len(dungeon), len(dungeon[0])
-        dp = {}
-        def dfs(i, j):
-            if i >= M or j >= N:
-                return -2000
-            if (i, j) in dp:
-                return dp[(i, j)]
-            if i == M - 1 and j == N - 1:
-                return dungeon[i][j]
-            down = dfs(i + 1, j)
-            right = dfs(i, j + 1)
-            curr = max(down, right) 
-            if curr >= 0:
-                curr = 0
-            dp[(i, j)] = curr + dungeon[i][j]            
-            return dp[(i, j)]
-        power_needed = dfs(0, 0) 
-        if power_needed >= 0:
-            return 1
-        return power_needed * -1 + 1
-            
+        for i in range(M - 1, -1, -1):
+            for j in range(N - 1, -1, -1):
+                if i == M - 1 and j == N - 1:
+                    continue
+                if i + 1 < M and j + 1 < N:
+                    need = max(dungeon[i + 1][j], dungeon[i][j + 1])                    
+                elif i + 1 < M:
+                    need = max(dungeon[i + 1][j], -2000)
+                else:
+                    need = max(-2000, dungeon[i][j + 1])
+                if need < 0:
+                    dungeon[i][j] += need
+
+
+        return 1 if dungeon[0][0] >= 0 else (dungeon[0][0] * -1 + 1)
