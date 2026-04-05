1caef514b5b2bae5d7580a9036a83d091370f6c9
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-04 21:04:43 -0400

    Time: 1 ms (87.52%), Space: 47.3 MB (64.88%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,42 +1,24 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        // [1,0,0,1],
-        // [0,1,1,0],
-        // [0,1,1,1],
-        // [1,0,1,1]
-        // [0,1,2,3,4]
-        //  0,1,2,3,4
-        int n = isConnected.length;
-        int[] parent = new int[n+1];
-        for(int i=1; i<=n; i++){
-            parent[i] = i;
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j] == 1){
-                    union(parent, i+1, j+1);
-                }
+    public int findCircleNum(int[][] graph) {
+        int n = graph.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(graph, i, visited);
+                provinces++;
             }
         }
-        int count = 0;
-        for(int i=1; i<=n; i++){
-            if(find(parent, i) == i) count++;
-        }
-        return count;
+        return provinces;        
     }
 
-    private int find(int[] parent, int x){
-        while(parent[x] != x){
-            x = parent[x];
-        }
-        return x;
-    }
-
-    private void union(int[] parent, int a, int b){
-        int rootA = find(parent, a);
-        int rootB = find(parent, b);
-        if(rootA != rootB){
-            parent[rootB] = rootA;
+    private void dfs(int[][] graph, int city, boolean[] visited) {
+        if (visited[city]) return;
+        visited[city] = true;
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[city][i] == 1) {
+                dfs(graph, i, visited);
+            }
         }
     }
 }
