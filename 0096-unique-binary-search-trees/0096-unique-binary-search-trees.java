c4d7eae2fcafce02de2b7a7d76b27b99633f1e26
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-21 13:01:38 -0400

    Time: 0 ms (100%), Space: 41.9 MB (71.11%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,17 +1,13 @@
 class Solution {
-    Integer[] dp;
     public int numTrees(int n) {
-        dp = new Integer[n + 1];
-        return dfs(n);
-    }
-
-    private int dfs(int n) {
-        if (n < 2) return 1;
-        if (dp[n] != null) return dp[n];
-        int curr = 0;
-        for (int k = 1; k <= n; k++) {
-            curr += dfs(k - 1) * dfs(n - k);
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                dp[i] += dp[k - 1] * dp[i - k];
+            }
         }
-        return dp[n] = curr;
+        return dp[n];
     }
 }
