ae4a6a7ef6ca03fd9033779549feabb0ec786af2
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-23 20:42:13 -0400

    Time: 6 ms (6.93%), Space: 47.4 MB (31.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,24 +1,41 @@
 class Solution {
-    public int findCircleNum(int[][] graph) {
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-        int provinces = 0;
+    public int findCircleNum(int[][] isConnected) {
+        //  1 2 3
+        // [1,1,0] 1
+        // [1,1,0] 2
+        // [0,0,1] 3
+
+        // [0, 1, 1, 3]
+        int n = isConnected.length;
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(graph, i, visited);
-                provinces++;
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    union(i, j, parent);
+                }
             }
         }
-        return provinces;        
+
+        int p = 0;
+        for (int i = 0; i < parent.length; i++) {
+            if (find(i, parent) == i) p++;
+        }
+        return p;
     }
 
-    private void dfs(int[][] graph, int city, boolean[] visited) {
-        if (visited[city]) return;
-        visited[city] = true;
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[city][i] == 1) {
-                dfs(graph, i, visited);
-            }
+    private int find(int node, int[] parent) {
+        while (parent[node] != node) {
+            node = parent[node];
         }
+        return node;
     }
+
+    private void union(int u, int v, int[] parent) {
+        int pu = find(u, parent);
+        int pv = find(v, parent);
+        if (pu == pv) return;
+        parent[pu] = pv;
+    }
+    
 }
