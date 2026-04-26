ae03308968d369a51f0b8392f246a8bc03cfa1fa
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-25 21:38:05 -0400

    Time: 69 ms (23.84%), Space: 46.4 MB (30.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,16 +1,23 @@
 class Solution {
+    Integer[] dp;
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[nums.length - 1] = 1;
-        for(int i = nums.length - 2; i >= 0; i--) {
-            int maxVal = 0;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] > nums[i]) {
-                    maxVal = Math.max(maxVal, dp[j]);
-                }
-            }
-            dp[i] = maxVal + 1;
+        dp = new Integer[nums.length];
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            res = Math.max(res, dfs(nums, i));
         }
-        return Arrays.stream(dp).max().getAsInt();
+        return res;
+    }
+
+    private int dfs(int[] nums, int start) {
+        if (start == nums.length) return 0;
+        if (dp[start] != null) return dp[start];
+        int curr = Integer.MIN_VALUE;
+        for (int i = start + 1; i < nums.length; i++) {
+            if (nums[i] > nums[start]) {
+                curr = Math.max(curr, dfs(nums, i));
+            }
+        }        
+        return dp[start] = (curr == Integer.MIN_VALUE ? 0 : curr) + 1;
     }
 }
