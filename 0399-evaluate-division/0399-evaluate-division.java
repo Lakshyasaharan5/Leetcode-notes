f51abc8abcae77811b6c3c89294d41b22cd1b86c
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-25 23:12:05 -0400

    Time: 1 ms (97.13%), Space: 46.8 MB (65.57%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,64 +1,52 @@
 class Edge {
     String to;
-    double weight;
-    public Edge(String to, double weight) {
+    double w;
+    public Edge(String to, double w) {
         this.to = to;
-        this.weight = weight;
+        this.w = w;
     }
 }
 
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        HashMap<String, ArrayList<Edge>> graph = buildGraph(equations, values);
+        int n = values.length;
+        Map<String, List<Edge>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String u = equations.get(i).get(0);
+            String v = equations.get(i).get(1);
+            double w = values[i];
+            graph.putIfAbsent(u, new ArrayList<>());
+            graph.putIfAbsent(v, new ArrayList<>());
+            graph.get(u).add(new Edge(v, w));
+            graph.get(v).add(new Edge(u, 1/w));
+        }
         double[] res = new double[queries.size()];
-        int k = 0;
-        for (List<String> query : queries) {
-            String u = query.get(0);
-            String v = query.get(1);
-            if (!graph.containsKey(u) || !graph.containsKey(v)) {
-                res[k] = (double)-1;
-            } else {
-                Set<String> visited = new HashSet<>();
-                res[k] = dfs(graph, u, v, visited);
-            }
-            k++;
+        for (int i = 0; i < queries.size(); i++) {
+            res[i] = bfs(graph, queries.get(i).get(0), queries.get(i).get(1));
         }
         return res;
     }
 
-    private double dfs(HashMap<String, ArrayList<Edge>> graph, String src, String dest, Set<String> visited) {
-        if (src.equals(dest)) {
-            return 1.0;
-        }
-        
+    private double bfs(Map<String, List<Edge>> graph, String src, String dst) {
+        if (!graph.containsKey(src) || !graph.containsKey(dst)) return -1.0;
+        if (src.equals(dst)) return 1.0;
+        Queue<Edge> q = new LinkedList<>();        
+        Set<String> visited = new HashSet<>();
+        q.offer(new Edge(src, 1));       
         visited.add(src);
-        for (Edge edge : graph.get(src)) {
-            String nb = edge.to;
-            if (!visited.contains(nb)){
-                double prod = dfs(graph, edge.to, dest, visited) * edge.weight;
-                if (prod >= 0) {
-                    return prod;
-                }
-            }            
+        while (!q.isEmpty()) {
+            Edge curr = q.poll();
+            String u = curr.to;
+            double division = curr.w;
+            for (Edge neib : graph.get(u)) {
+                String v = neib.to;
+                double w = neib.w;
+                if (visited.contains(v)) continue;
+                visited.add(v);
+                if (v.equals(dst)) return division * w;
+                q.offer(new Edge(v, division * w));
+            }
         }
         return -1.0;
-    }
-
-    private HashMap<String, ArrayList<Edge>> buildGraph(List<List<String>> equations, double[] values) {
-        HashMap<String, ArrayList<Edge>> graph = new HashMap<String, ArrayList<Edge>>();
-        for (int i = 0; i < equations.size(); i++) {
-            String u = equations.get(i).get(0);
-            String v = equations.get(i).get(1);
-            double w = values[i];
-            if (!graph.containsKey(u)) {
-                graph.put(u, new ArrayList<Edge>());
-            }
-            if (!graph.containsKey(v)) {
-                graph.put(v, new ArrayList<Edge>());
-            }
-            graph.get(u).add(new Edge(v, w));
-            graph.get(v).add(new Edge(u, 1/w));
-        }
-        return graph;
     }
 }
