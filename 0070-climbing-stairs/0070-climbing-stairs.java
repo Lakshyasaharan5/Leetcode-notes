fd9572b7bcd2e252f86b7dcdff9c55584d7335e3
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-27 12:03:10 -0400

    Time: 0 ms (100%), Space: 42.1 MB (54.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,21 @@
 class Solution {
+    Integer[] dp;
     public int climbStairs(int n) {
-        int a = 1, b = 0, c = 0;
-        while (n-- > 0) {
-            c = a + b;
-            b = a;
-            a = c;
-        }
-        return c;
+        //            0
+        //          /   \
+        //         1     2
+        //       /   \
+        //      2     3 
+        //     / \
+        //    3   4 
+        dp = new Integer[n + 1];
+        return dfs(n, 0);
+    }
+
+    private int dfs(int n, int i) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+        if (dp[i] != null) return dp[i];
+        return dp[i] = dfs(n, i + 1) + dfs(n, i + 2);
     }
 }
