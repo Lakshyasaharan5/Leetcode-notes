1bb8173517aa3c1f077c71de7f895276dff15ef8
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-28 12:55:05 -0400

    Time: 4 ms (4.76%), Space: 43.6 MB (5.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,19 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[n] = 1;
-        dp[n - 1] = (s.charAt(n - 1) == '0' ? 0 : 1);
-        for (int i = n - 2; i >= 0; i--) {
-            int oneChar = Integer.parseInt(s.charAt(i) + "");
-            int twoChar = Integer.parseInt(s.charAt(i) + "" + s.charAt(i + 1));
-            if (oneChar == 0) {
-                dp[i] = 0;
-                continue;
+        int a = s.charAt(n - 1) == '0' ? 0 : 1;
+        int b = 1;
+        for (int i = n - 2; i >=0; i--) {
+            int c = 0;
+            int one = Integer.parseInt(s.charAt(i) + "");
+            int two = Integer.parseInt(s.charAt(i) + "" + s.charAt(i + 1));
+            if (one == 0) {
+                c = 0;
+            } else {
+                c = a;
+                if (two <= 26) {
+                    c += b;
+                }
             }
-            dp[i] = dp[i + 1];
-            if (twoChar <= 26) dp[i] += dp[i + 2];
-        }
-        return dp[0];
-    }
+            b = a;
+            a = c;
+        } 
+        return a;
+     }
 }
