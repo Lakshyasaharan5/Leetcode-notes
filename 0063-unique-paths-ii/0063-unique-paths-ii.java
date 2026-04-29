68e78932e690c68a6cef3817884052d2d1f0c1b3
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-04-29 12:36:33 -0400

    Time: 0 ms (100%), Space: 43.7 MB (26.99%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,15 +1,21 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    //    1 1 % 1
+    //   [1 1 1 *]
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[] dp = new int[n];
-        dp[n - 1] = 1;
+        dp[n - 1] = obstacleGrid[m - 1][n - 1] == 1 ? 0 : 1;
+
         for (int i = m - 1; i >= 0; i--) {
-            if (obstacleGrid[i][n - 1] == 1) dp[n - 1] = 0;
-            for (int j = n - 2; j >= 0; j--) {
-                if (obstacleGrid[i][j] == 1) dp[j] = 0;
-                else dp[j] += dp[j + 1];
+            for (int j = n - 1; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                    continue;
+                }
+                dp[j] += (j < n - 1 ? dp[j + 1] : 0);
             }
         }
+
         return dp[0];
     }
 }
