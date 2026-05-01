8b4ba89f7dc60ec73613fc4258104843208a4008
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-05-01 17:10:32 -0400

    Time: 1 ms (62.2%), Space: 47.1 MB (77.29%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,28 +1,32 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        Set<Integer> red = new HashSet<Integer>();
-        Set<Integer> green = new HashSet<Integer>();
+        int[] color = new int[n];
         for (int i = 0; i < n; i++) {
-            if (red.contains(i) || green.contains(i)) continue;
-            if (!dfs(graph, i, true, red, green)) return false;
+            if (color[i] == 0) {
+                if (!bfs(i, color, graph)) return false;
+            }
         }
-        return true;                
+        return true;
     }
 
-    private boolean dfs(int[][] graph, int node,
-            boolean isRed, Set<Integer> red, Set<Integer> green) {
-        if (isRed) {
-            if (green.contains(node)) return false;
-            if (red.contains(node)) return true;
-            red.add(node);
-        } else {
-            if (red.contains(node)) return false;
-            if (green.contains(node)) return true;            
-            green.add(node);
-        }
-        for (int neib : graph[node]) {
-            if (!dfs(graph, neib, !isRed, red, green)) return false;
+    private boolean bfs (int start, int[] color, int[][] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        color[start] = 1;
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            int currColor = color[curr];
+            for (int nb : graph[curr]) {
+                if (color[nb] == 0) {
+                    color[nb] = -1 * currColor;
+                    q.offer(nb);
+                    continue;
+                }
+                if (color[nb] != -1 * currColor) {
+                    return false;
+                }
+            }
         }
         return true;
     }
