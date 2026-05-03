5f39c7bd90abbe748966835e6cc532a8557dcbce
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-05-03 14:22:31 -0400

    Time: 22 ms (39.9%), Space: 47 MB (67.19%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,42 +1,30 @@
 class Solution {
     public int minCost(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] dist = new int[m][n];
-
-        for (int i = 0; i < m; i++)
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-
-        dist[0][0] = 0;
-
+        int[][] d = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(d[i], Integer.MAX_VALUE);
+        }
+        d[0][0] = 0;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.offer(new int[]{0, 0, 0}); // {cost, i, j}
-
-        int[][] dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
+        pq.offer(new int[]{0, 0, 0}); // w, i, j
+        int[][] DIRECTIONS = new int[][]{{-1, -1}, {0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while (!pq.isEmpty()) {
             int[] curr = pq.poll();
-            int cost = curr[0];
-            int i = curr[1], j = curr[2];
-
-            if (cost > dist[i][j]) continue;
-
-            for (int d = 0; d < 4; d++) {
-                int r = i + dirs[d][0];
-                int c = j + dirs[d][1];
-
-                if (r < 0 || c < 0 || r >= m || c >= n) continue;
-
-                // direction mapping:
-                // 1 → right, 2 → left, 3 → down, 4 → up
-                int nextWeight = (grid[i][j] == d + 1) ? 0 : 1;
-
-                if (dist[i][j] + nextWeight < dist[r][c]) {
-                    dist[r][c] = dist[i][j] + nextWeight;
-                    pq.offer(new int[]{dist[r][c], r, c});
+            int cost = curr[0];            
+            int r = curr[1], c = curr[2];
+            if (cost > d[r][c]) continue;
+            for (int i = 1; i <= 4; i++) {
+                int[] dir = DIRECTIONS[i];
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+                if (nr < 0 || nc < 0 || nr >= m || nc >= n) continue;               
+                if (cost + (grid[r][c] == i ? 0 : 1) < d[nr][nc]) {
+                    d[nr][nc] = cost + (grid[r][c] == i ? 0 : 1);
+                    pq.offer(new int[]{d[nr][nc], nr, nc});
                 }
             }
         }
-
-        return dist[m - 1][n - 1];
+        return d[m - 1][n - 1];
     }
 }
