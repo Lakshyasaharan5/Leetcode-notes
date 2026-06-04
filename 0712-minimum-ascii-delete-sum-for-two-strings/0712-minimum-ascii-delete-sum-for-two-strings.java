74d79d1174e8d4bf5fc570feb6da952227d8de2f
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-06-04 17:31:55 -0400

    Time: 30 ms (8.98%), Space: 49.7 MB (5.44%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,28 +1,39 @@
 class Solution {
+    Integer[][] dp;
     public int minimumDeleteSum(String s1, String s2) {
-        int m = s1.length(), n = s2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        dp[m][n] = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            dp[i][n] = dp[i + 1][n] + s1.charAt(i);
-        }
-        for (int j = n - 1; j >= 0; j--) {
-            dp[m][j] = dp[m][j + 1] + s2.charAt(j);
-        }
+        dp = new Integer[s1.length() + 1][s2.length() + 1];
+        return dfs(s1, s2, 0, 0);
+    }
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (s1.charAt(i) == s2.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j + 1];
-                } else {
-                    dp[i][j] = Math.min(
-                        dp[i + 1][j] + s1.charAt(i),
-                        dp[i][j + 1] + s2.charAt(j)
-                    );
-                }
+    private int dfs(String s1, String s2, int i, int j) {
+        if (i >= s1.length() && j >= s2.length()) return 0;
+        if (dp[i][j] != null) return dp[i][j];
+        if (i >= s1.length()) {
+            int asciiValue = 0;
+            for (char ch : s2.substring(j).toCharArray()) {
+                asciiValue += ascii(ch);
             }
+            return asciiValue;
+        }
+        if (j >= s2.length()) {
+            int asciiValue = 0;
+            for (char ch : s1.substring(i).toCharArray()) {
+                asciiValue += ascii(ch);
+            }
+            return asciiValue;
+        }        
+        int l = Integer.MAX_VALUE, c = Integer.MAX_VALUE, r = Integer.MAX_VALUE;
+        if (s1.charAt(i) == s2.charAt(j)) {
+            c = dfs(s1, s2, i + 1, j + 1);
+        } else {
+            l = dfs(s1, s2, i + 1, j) + ascii(s1.charAt(i));
+            r = dfs(s1, s2, i, j + 1) + ascii(s2.charAt(j));
         }
 
-        return dp[0][0];
+        return dp[i][j] = Math.min(Math.min(l, r), c);
+    }
+
+    private int ascii(char ch) {
+        return (int)ch;
     }
 }
