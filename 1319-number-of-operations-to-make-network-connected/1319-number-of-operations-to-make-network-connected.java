147a5a4e90213dfe7d5e4133e2eef2e5f80658cc
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-06-05 16:40:31 -0400

    Time: 17 ms (20.64%), Space: 65 MB (98%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,30 +1,28 @@
 class Solution {
+    int components = 0;
     public int makeConnected(int n, int[][] connections) {
-        if (connections.length < n - 1) return -1;
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) graph.put(i, new ArrayList<>());
-        for (int[] edge : connections) {
-            int u = edge[0], v = edge[1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+        int cables = connections.length;
+        int extra = cables - (n - 1);
+        if (extra < 0) return -1;
+        int[] parents = new int[n]; 
+        for (int i = 0; i < n; i++) parents[i] = i;
+        components = n;       
+        for (int[] c : connections) {
+            union(parents, c[0], c[1]);
         }
-        boolean[] visited = new boolean[n];
-        int components = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(graph, i, visited);
-                components++;            
-            }
-        }
-            
         return components - 1;
     }
 
-    private void dfs(Map<Integer, List<Integer>> graph, int start, boolean[] visited) {
-        if (visited[start]) return;
-        visited[start] = true;
-        for (int neib : graph.get(start)) {
-            dfs(graph, neib, visited);
-        }
+    private void union(int[] parents, int u, int v) {
+        int pu = find(parents, u);
+        int pv = find(parents, v);
+        if (pu == pv) return;
+        components--;
+        parents[pv] = pu;
+    }
+
+    private int find(int[] parents, int u) {
+        while (parents[u] != u) u = parents[u];
+        return u;
     }
 }
