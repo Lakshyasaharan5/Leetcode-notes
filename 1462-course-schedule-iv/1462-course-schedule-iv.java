24ff3f7023aef50c2c3e901c15a971a85d8fb8f0
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-06-05 16:06:14 -0400

    Time: 51 ms (37.61%), Space: 51.4 MB (19.59%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,31 +1,36 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        int n = numCourses;
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) graph.putIfAbsent(i, new ArrayList<>());
+        for (int i = 0; i < numCourses; i++) 
+            graph.put(i, new ArrayList<>());
         for (int[] pre : prerequisites) {
-            int u = pre[0], v = pre[1];
-            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);            
-        }        
-
-        boolean[][] reachable = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            dfs(i, i, graph, reachable);
+            graph.get(pre[0]).add(pre[1]);
         }
-
+        Map<Integer, Set<Integer>> preMap = new HashMap<>();
+        for (int i = 0; i < numCourses; i++)  {
+            if (!preMap.containsKey(i)) {
+                dfs(graph, i, preMap);
+            }
+        }
         List<Boolean> res = new ArrayList<>();
         for (int[] q : queries) {
-            if (reachable[q[0]][q[1]]) res.add(true);
-            else res.add(false);
+            int u = q[0], v = q[1];
+            if(preMap.get(u).contains(v))
+                res.add(true);
+            else
+                res.add(false);
         }
         return res;
     }
 
-    private void dfs(int src, int dst, Map<Integer, List<Integer>> graph, boolean[][] reachable) {
-        if (reachable[src][dst]) return;
-        for (int nb : graph.get(dst)) {            
-            dfs(src, nb, graph, reachable);
-            reachable[src][nb] = true;
+    private void dfs(Map<Integer, List<Integer>> graph, int node, Map<Integer, Set<Integer>> preMap) {    
+        if (preMap.containsKey(node)) return;
+        Set<Integer> curr = new HashSet<>();
+        for (int nextCourse : graph.get(node)) {
+            dfs(graph, nextCourse, preMap);
+            curr.add(nextCourse);
+            curr.addAll(preMap.get(nextCourse));
         }
+        preMap.put(node, curr);
     }
 }
