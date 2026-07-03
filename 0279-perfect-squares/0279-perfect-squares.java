0388b7d0dd755e538cc6748359de66b565576267
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-03 17:24:43 -0400

    Time: 34 ms (56.03%), Space: 44.6 MB (52.1%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -4,11 +4,11 @@ class Solution {
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 1; i < n + 1; i++) {
-            int curr = i;
-            for (int j = 1; j * j <= i; j++) {
-                curr = Math.min(curr, dp[i - j * j]);
+            int j = 1;
+            while (i - j * j >= 0) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                j++;
             }
-            dp[i] = curr + 1;
         }
         return dp[n];
     }
