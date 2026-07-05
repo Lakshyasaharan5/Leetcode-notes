7ac731e98370a556ed671fcd4634134350f93d69
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-05 14:46:36 -0400

    Time: 36 ms (30.88%), Space: 58.8 MB (90.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
         Arrays.sort(nums);
-        // -4 -1 -1 0 1 2
-        //     l 
-        //            r
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int target = -1 * nums[i];
-            int l = i + 1, r = nums.length - 1;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = 0 - nums[i];
+            int l = i + 1, r = n - 1;
             while (l < r) {
-                int sum = nums[l] + nums[r];
-                if (sum == target) {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));                    
-                }
-                if (sum > target){
-                    r--;
-                    while (l < r && nums[r] == nums[r + 1]) r--;                                        
-                }
-                else{
-                    l++;                
+                if (nums[l] + nums[r] == target) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r])));
                     while (l < r && nums[l] == nums[l - 1]) l++;
-                }
-               
+                    r--;
+                    while (r > l && nums[r] == nums[r + 1]) r--;
+                } else if (nums[l] + nums[r] < target) {
+                    l++;
+                } else {
+                    r--;
+                }                
             }
         }
         return res;
+        /**      i        
+             -4 -1 -1 0 1 2
+                    l
+                          r
+
+            if (found)
+                l++
+                r--
+            else if l + r < target
+                l++
+            else 
+                r--
+
+        */
     }
 }
