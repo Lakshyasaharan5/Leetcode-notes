33b11d047eeefdc750644e051feaae1c25bab7eb
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-05 14:23:37 -0400

    Time: 1 ms (60.1%), Space: 47.7 MB (68.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,36 +1,37 @@
 class Solution {
     public int trap(int[] height) {
-        int l = 0, r = height.length - 1;
-        int lmax = l, rmax = r;
+        int n = height.length;
+        int l = 0, r = n - 1;
+        int lmax = height[0], rmax = height[r];
         int water = 0;
-        while (l < r) {
-            if (height[l] <= height[r]) {
+        while (l < r) {            
+            if (height[l] < height[r]) {
+                lmax = Math.max(lmax, height[l]);
+                water += lmax - height[l];
                 l++;
-                if (height[l] > height[lmax]) {
-                    lmax = l;
-                } else {
-                    water += height[lmax] - height[l];
-                }
             } else {
+                rmax = Math.max(rmax, height[r]);
+                water += rmax - height[r];
                 r--;
-                if (height[r] > height[rmax]) {
-                    rmax = r;
-                } else {
-                    water += height[rmax] - height[r];
-                }
             }
         }
         return water;
-
-        //               3
-        //       2 # # # 3 2 # 2
-        // _,1,#,2,1,#,1,3,2,1,2,1
-        //               L           
-        //             R
-        //               Lmax                  
-        //                     Rmax
-
-        //    water += R-L+1 (1 + 1 + 2 + 1 + 1)
-        
+        /**
+                             r
+         l
+        [1,1,0,2,1,0,1,3,2,1,2,1]
+                       m     
+                         m
+           water = 6
+           1 # 2 1 # 1 3 2 1 2 1
+               2 # # # 3 2 # 2
+                       3 
+            
+            $          
+            $   $  $     
+            $...$..$...$....
+            l
+                       r   
+        */
     }
 }
