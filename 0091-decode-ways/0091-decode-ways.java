633c88969fa5a65a9d8f50804287706d93afc7f7
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-06 16:48:30 -0400

    Time: 1 ms (80.89%), Space: 43 MB (63.99%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,5 +1,18 @@
 class Solution {
     public int numDecodings(String s) {
+        /**                      
+            101
+            012
+             0  
+           /    \     
+          ~1       2  
+        /   \    /  \
+       2     3   3   4
+      / \
+            if i==len: return 1
+            if i>len: return max
+            if s[i] == 0 return max
+         */
         int[] dp = new int[s.length()];
         Arrays.fill(dp, -1);
         return dfs(s, 0, dp);
@@ -7,12 +20,12 @@ class Solution {
 
     private int dfs(String s, int i, int[] dp) {
         if (i == s.length()) return 1;
-        if (s.charAt(i) == '0') return 0;
+        if (i > s.length() || s.charAt(i) == '0') return 0;
         if (dp[i] != -1) return dp[i];
-        int left = dfs(s, i + 1, dp);        
-        int right = 0;
-        if (i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26) 
-            right = dfs(s, i + 2, dp);
-        return dp[i] = left + right;
+        int singleDigit = dfs(s, i + 1, dp);
+        int doubleDigit = 0;
+        if (i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26)
+            doubleDigit = dfs(s, i + 2, dp);
+        return dp[i] = singleDigit + doubleDigit;
     }
 }
