f938b6db4c4e202d586bcad7fb05eeec80b545d5
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-09 13:57:05 -0400

    Time: 4 ms (16.21%), Space: 49.9 MB (6.43%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,57 +1,41 @@
 class Solution {
-    int components = 0;
-    int[] rank;
     public int findCircleNum(int[][] isConnected) {
-        //  1 2 3
-        // [1,1,0] 1
-        // [1,1,0] 2
-        // [0,0,1] 3
-
-        // [0, 1, 1, 3]
         int n = isConnected.length;
-        this.components = n;
-        int[] parent = new int[n];
-        rank = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {            
             for (int j = i + 1; j < n; j++) {
                 if (isConnected[i][j] == 1) {
-                    union(i, j, parent);
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
                 }
             }
         }
-
-        int p = 0;
-        for (int i = 0; i < parent.length; i++) {
-            if (find(i, parent) == i) p++;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {                
+                dfs(graph, i, visited);
+                provinces++;
+            }            
         }
-        return this.components;
+        return provinces;
+        /**
+               0 1 2
+             0[1,1,0]
+             1[1,1,0]
+             2[0,0,1]                     
+         */
+
     }
 
-    /*
-             4
-             \ \
-              1 2
-               \
-                 2
-     */
-    private int find(int node, int[] parent) {
-        if (parent[node] == node) return node;
-        parent[node] = find(parent[node], parent);
-        return parent[node];
-    }
-
-    private void union(int u, int v, int[] parent) {
-        int pu = find(u, parent);
-        int pv = find(v, parent);
-        if (pu == pv) return;
-        if (rank[pu] > rank[pv]) {
-            parent[pv] = pu;            
-        } else {
-            parent[pu] = pv;
-            rank[pv]++;      
+    private void dfs(Map<Integer, List<Integer>> graph, int start, boolean[] visited) {
+        if (visited[start]) return;
+        visited[start] = true;
+        for (int nb : graph.get(start)) {
+            dfs(graph, nb, visited);
         }
-        this.components--;
     }
-    
 }
