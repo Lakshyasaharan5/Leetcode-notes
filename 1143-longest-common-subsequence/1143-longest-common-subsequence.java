70d833e1347c9edb9c7841bd8e45dffbfde16461
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-09 11:40:01 -0400

    Time: 28 ms (19.6%), Space: 54.1 MB (19.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,19 +1,30 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        // abc and ade
-        // m = 3, n = 3
-        int m = text1.length(), n = text2.length();
-        // dp 0s
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                // a a
-                if (text1.charAt(i) == text2.charAt(j))
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                else // 
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
-            }
+        /**
+                a c s d k f 
+                d a r d a d
+
+                i,j
+            i+1,j   i,j+1    (i+1,j+1)+1               
+        
+        */
+        int[][] dp = new int[text1.length()][text2.length()];
+        for (int i = 0; i < text1.length(); i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return dp[0][0];
+        return dfs(text1, text2, 0, 0, dp);
+    }
+
+    private int dfs(String a, String b, int i, int j, int[][] dp) {
+        if (i == a.length() || j == b.length())
+            return 0;
+        
+        if (dp[i][j] != -1) return dp[i][j];
+        if (a.charAt(i) == b.charAt(j)) {
+            return 1 + dfs(a, b, i + 1, j + 1, dp);
+        }
+
+        return dp[i][j] = Math.max(dfs(a, b, i + 1, j, dp), 
+                                    dfs(a, b, i, j + 1, dp));
     }
 }
