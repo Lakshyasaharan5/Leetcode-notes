cdbeeee0f74ad62bf05423a86830ce5abf285a34
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-09 12:14:34 -0400

    Time: 6 ms (8.91%), Space: 47.2 MB (53.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,22 +1,44 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        /**
+            a b 
+            c 
+            min(del, rep, ins)
+            min(i+1,j    i+1,j+1,     i,j+1)
+                       (ab, c)
+            d/             r|               i\
+          b,c              cb,c             cab,c
+    '',c  c,c 
+
+            a b c d
+                    i
+            s d a c 
+                j
+
+        */
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = m - 1; i >= 0; i--) {
-            dp[i][n] = 1 + dp[i + 1][n];
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++)
+            Arrays.fill(dp[i], -1);
+        return dfs(word1, word2, 0, 0, dp);
+    }
+
+    private int dfs(String a, String b, int i, int j, int[][] dp) {
+        if (i == a.length() && j == b.length()) 
+            return 0;
+        if (i == a.length()) {
+            return b.length() - j;
         }
-        for (int j = n - 1; j >= 0; j--) {
-            dp[m][j] = 1 + dp[m][j + 1];
+        if (j == b.length()) {
+            return a.length() - i;
         }
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (word1.charAt(i) == word2.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j + 1];
-                } else {
-                    dp[i][j] = 1 + Math.min(dp[i + 1][j], Math.min(dp[i][j + 1], dp[i + 1][j + 1]));
-                }
-            }
+        if (dp[i][j] != -1) return dp[i][j];
+        if (a.charAt(i) == b.charAt(j)) {
+            return dfs(a, b, i + 1, j + 1, dp);
         }
-        return dp[0][0];
+        int del = dfs(a, b, i + 1, j, dp);
+        int rep = dfs(a, b, i + 1, j + 1, dp);
+        int ins = dfs(a, b, i, j + 1, dp);
+        return dp[i][j] = Math.min(Math.min(del, rep), ins) + 1;
     }
 }
