2c05134e7454bc09c41cc7fe1f1fb0e47d153b6b
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-10 17:10:34 -0400

    Time: 50 ms (84.87%), Space: 77 MB (77.92%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,23 +1,37 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<int[]> stack = new Stack<>();
-        stack.push(new int[] { 0, heights[0] });
-        int area = 0;
+        Stack<int[]> st = new Stack<>();
+        st.push(new int[]{0, 0});
+        int maxArea = Integer.MIN_VALUE;
         for (int i = 1; i < heights.length; i++) {
             int left = i;
-            while (!stack.isEmpty() && stack.peek()[1] >= heights[i]) {
-                int[] top = stack.pop();
-                area = Math.max(area, (i - top[0]) * top[1]);
-                left = top[0];
+            while (!st.isEmpty() && heights[st.peek()[0]] >= heights[i]) {
+                int[] top = st.pop();  // (curr index, how far left)
+                int currIndex = top[0];
+                left = top[1];         
+                maxArea = Math.max(maxArea, (i - left) * heights[currIndex]);       
             }
-            stack.push(new int[] { left, heights[i] });
-
+            st.push(new int[]{i, left});
         }
         int right = heights.length;
-        while (!stack.isEmpty()) {
-            int[] top = stack.pop();
-            area = Math.max(area, (right - top[0]) * top[1]);                
+        while (!st.isEmpty()) {
+            int[] top = st.pop();
+            maxArea = Math.max(maxArea, (right - top[1]) * heights[top[0]]);
         }
-        return area;
+        return maxArea;
+        
+        /**
+            1 1 1 1 1
+                i
+            st = []
+             0 1 2 3 4 5
+            [2,1,5,6,2,3]
+                       i
+                     left = 
+            st = [(1,0)(4,2)(5,5)]
+            (curr index, how far left)
+            while pop()
+                max area = (i - left) * h[curr]
+         */
     }
 }
