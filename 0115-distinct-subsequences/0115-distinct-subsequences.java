3ac7bf1a7fbcb8b55d16f2c4611ba6105433ca52
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-11 15:13:11 -0400

    Time: 21 ms (35.95%), Space: 54.6 MB (31.11%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,19 +1,38 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int m = s.length(), n = t.length();
-        int[] dp = new int[n + 1];
-        dp[n] = 1;
-        for (int i = m - 1; i >= 0; i--) {
-            int[] next = new int[n + 1];
-            next[n] = 1;
-            for (int j = n - 1; j >= 0; j--) {
-                next[j] = dp[j];
-                if (s.charAt(i) == t.charAt(j)) {
-                    next[j] += dp[j + 1];
-                }
-            }
-            dp = next;
+        /**
+                s = bxxxa
+                       i
+                t = axxxb
+                      j
+
+                if equal
+                    return i+1,j+1 + i+1,j
+                else
+                    return i+1,j
+                    00
+              1/          1\
+              11            10
+                        1/        \0
+                        21        20
+
+                return left + right
+
+         */
+        int[][] dp = new int[s.length()][t.length()];
+        for (int i = 0; i < s.length(); i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return dp[0];
+        return dfs(s, t, 0, 0, dp);
+    }
+
+    private int dfs(String s, String t, int i, int j, int[][] dp) {
+        if (j == t.length()) return 1;
+        if (i == s.length()) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s.charAt(i) == t.charAt(j)) {
+            return dp[i][j] = dfs(s, t, i + 1, j + 1, dp) + dfs(s, t, i + 1, j, dp);
+        } 
+        return dp[i][j] = dfs(s, t, i + 1, j, dp);
     }
 }
