63a76c591944fc283c276689122d991027244f09
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-15 13:29:24 -0400

    Time: 4 ms (2.4%), Space: 41.9 MB (70.61%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,17 +1,47 @@
 class Solution {
-    Boolean[] dp;
     public boolean divisorGame(int n) {
-        dp = new Boolean[1001];
-        return canWin(n);
-    }
+        /**
+                4
+            1       2      alice
+           none     1      bob
+                    none   alice
 
-    private boolean canWin(int n) {
-        if (dp[n] != null) return dp[n];        
-        for (int i = 1; i < n; i++) {
-            if (n % i == 0 && !canWin(n - i)) {
-                return dp[n] = true;
+                    [w]
+                  w     w
+                l  w   w  w
+            
+            for i in range (0 to n)
+                if dfs(n - i)
+                    return true
+            return false
+
+                        alice
+                l l w l w w l l 
+            [l l w w w l]
+             0 1 2 3 4 5
+
+            for i in (0 to n)
+                if n%i and dp[n - i] == lose
+                    dp[i] = win
+            
+         */
+        if (n < 2) return false;
+
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0 && !dp[i - j]) {
+                    dp[i] = true;
+                    break;
+                }                    
             }
         }
-        return dp[n] = false;
+        return dp[n];
+
+        // for (int i = 1; i < n; i++) {
+        //     if (n % i == 0 && !divisorGame(n - i))
+        //         return true;
+        // }
+        // return false;
     }
 }
