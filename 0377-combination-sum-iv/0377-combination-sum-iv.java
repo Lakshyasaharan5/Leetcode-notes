289d1a8f3be45a4df3777b83ec6022f690a75c0f
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-15 12:57:07 -0400

    Time: 0 ms (100%), Space: 42.7 MB (46.59%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,14 +1,34 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
+        /**
+            1, 2, 3
+           1   2    3
+        1 2 3  2 3    3
+
+                 [1,2]               target=2
+            /           \
+          1,1           2,0
+        1,0   2,-1
+
+                ways = 0
+                for i in (0 to n)
+                    ways += dfs(target - a[i])
+                return ways
+         */
         int[] dp = new int[target + 1];
-        dp[0] = 1;
-        for (int i = 1; i <= target; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i - nums[j] >= 0) {
-                    dp[i] += dp[i - nums[j]];
-                }
+        Arrays.fill(dp, -1);
+        return dfs(nums, target, dp);
+    }
+
+    private int dfs(int[] nums, int target, int[] dp) {
+        if (target == 0) return 1;
+        if (dp[target] != -1) return dp[target];
+        int ways = 0;
+        for (int n : nums) {
+            if (target >= n) {
+                ways += dfs(nums, target - n, dp);
             }
         }
-        return dp[target];
+        return dp[target] = ways;
     }
 }
