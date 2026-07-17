0753a154bde2a5696e34368de7ef1555c187144a
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-17 15:01:35 -0400

    Time: 5 ms (37.18%), Space: 46.5 MB (79.82%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,36 +1,18 @@
-class Solution {
-    Integer[] dp;
-    public int deleteAndEarn(int[] nums) {
-        Arrays.sort(nums);
-        /**
-              0 1 2 3 4 5 6 7
-             [2,2,3,3,3,4,6,6]
-                              i  
-            pick 2 and dfs([4,6,6])
-            skip 2 and dfs([3....])
-            max(pick, skip)
+class Solution {    
+    public int deleteAndEarn(int[] nums) {                
+        int[] dp = new int[10004];
+        for (int n : nums) {
+            dp[n] += n;
+        }
+        for (int i = 10001; i >= 1; i--) {
+            dp[i] = Math.max(dp[i]+dp[i+2], dp[i+1]);
+        }        
+        return dp[1];
+        /**            
+            dp[i] = max(dp[i]+dp[i+2], dp[i+1])
+            [0 0 0 0 0 0 12 6]
+             2,2,3,3,3,4,6 ,6             
         */
-        dp = new Integer[nums.length];
-        return dfs(nums, 0);
-    }
-
-    private int dfs(int[] nums, int start) {
-        if (start >= nums.length) return 0;
-        if (dp[start] != null) return dp[start];
-        //pick        
-        int i = start;
-        while (i < nums.length && nums[i] == nums[start]) {
-            i++;
-        }
-        int nextGreater = i;
-        int currTotal = nums[start] * (i - start);
-        while (i < nums.length && nums[i] == nums[start] + 1) {
-            i++;
-        }
-        int pick = dfs(nums, i) + currTotal;
-
-        //skip
-        int skip = dfs(nums, nextGreater);
-        return dp[start] = Math.max(pick, skip);
-    }
+        
+    }   
 }
