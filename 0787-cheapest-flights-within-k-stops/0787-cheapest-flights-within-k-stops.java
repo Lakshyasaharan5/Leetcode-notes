735cfad152af3bc2ea13aea2c6198578bb65d1b7
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-19 19:28:54 -0400

    Time: 6 ms (85.48%), Space: 46.7 MB (26%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,47 +1,21 @@
-class Edge {
-    int to;
-    int weight;
-    Edge(int t, int w) {
-        this.to = t;
-        this.weight = w;
-    }
-}
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dest, int k) {
-        /**
-        k = 0         
-        q = [(200,2), (700,3)]
-        front = (1,100)
-        d = [0   100  200   700]
-             0   1    2     3
-        */
-
-        Map<Integer, List<Edge>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
-        }
-        for (int[] e : flights) {
-            graph.get(e[0]).add(new Edge(e[1], e[2]));
-        }
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{src, 0});
-        while (k-- >= 0) {
-            int size = queue.size();
-            while (size-- > 0) {
-                int[] front = queue.poll();
-                int cost = front[1];
-                for (Edge nb : graph.get(front[0])) {
-                    if (cost + nb.weight < dist[nb.to]) {
-                        dist[nb.to] = cost + nb.weight;
-                        queue.offer(new int[]{nb.to, dist[nb.to]});
-                    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] d = new int[n];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        d[src] = 0;
+        while (k >= 0) {
+            int[] t = new int[n];
+            Arrays.fill(t, Integer.MAX_VALUE);
+            t[src] = 0;
+            for (int[] edge : flights) {
+                int u = edge[0], v = edge[1], cost = edge[2];
+                if (d[u] != Integer.MAX_VALUE) {
+                    t[v] = Math.min(t[v], d[u] + cost);
                 }
             }
+            d = t;
+            k--;
         }
-
-        return dist[dest] == Integer.MAX_VALUE ? -1 : dist[dest];
+         return d[dst] == Integer.MAX_VALUE ? -1 : d[dst];
     }
 }
