bd928ab4bafb715da2a5fc345bd135bf5dcaf449
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-19 19:28:44 -0400

    Time: 7 ms (39.44%), Space: 46.3 MB (84.33%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,82 +1,47 @@
-import java.util.*;
-
+class Edge {
+    int to;
+    int weight;
+    Edge(int t, int w) {
+        this.to = t;
+        this.weight = w;
+    }
+}
 class Solution {
-    public int findCheapestPrice(
-        int n,
-        int[][] flights,
-        int src,
-        int dst,
-        int k
-    ) {
-        List<int[]>[] graph = new ArrayList[n];
+    public int findCheapestPrice(int n, int[][] flights, int src, int dest, int k) {
+        /**
+        k = 0         
+        q = [(200,2), (700,3)]
+        front = (1,100)
+        d = [0   100  200   700]
+             0   1    2     3
+        */
 
+        Map<Integer, List<Edge>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+            graph.put(i, new ArrayList<>());
         }
-
-        for (int[] flight : flights) {
-            int from = flight[0];
-            int to = flight[1];
-            int price = flight[2];
-
-            graph[from].add(new int[]{to, price});
+        for (int[] e : flights) {
+            graph.get(e[0]).add(new Edge(e[1], e[2]));
         }
-
-        int maxFlights = k + 1;
-        int inf = Integer.MAX_VALUE;
-
-        // dist[node][flightsUsed]
-        int[][] dist = new int[n][maxFlights + 1];
-
-        for (int[] row : dist) {
-            Arrays.fill(row, inf);
-        }
-
-        // {cost, node, flightsUsed}
-        PriorityQueue<int[]> minHeap =
-            new PriorityQueue<>(Comparator.comparingInt(state -> state[0]));
-
-        dist[src][0] = 0;
-        minHeap.offer(new int[]{0, src, 0});
-
-        while (!minHeap.isEmpty()) {
-            int[] state = minHeap.poll();
-
-            int cost = state[0];
-            int node = state[1];
-            int flightsUsed = state[2];
-
-            if (cost != dist[node][flightsUsed]) {
-                continue;
-            }
-
-            if (node == dst) {
-                return cost;
-            }
-
-            if (flightsUsed == maxFlights) {
-                continue;
-            }
-
-            for (int[] edge : graph[node]) {
-                int neighbor = edge[0];
-                int price = edge[1];
-
-                int nextCost = cost + price;
-                int nextFlightsUsed = flightsUsed + 1;
-
-                if (nextCost < dist[neighbor][nextFlightsUsed]) {
-                    dist[neighbor][nextFlightsUsed] = nextCost;
-
-                    minHeap.offer(new int[]{
-                        nextCost,
-                        neighbor,
-                        nextFlightsUsed
-                    });
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{src, 0});
+        while (k-- >= 0) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] front = queue.poll();
+                int cost = front[1];
+                for (Edge nb : graph.get(front[0])) {
+                    if (cost + nb.weight < dist[nb.to]) {
+                        dist[nb.to] = cost + nb.weight;
+                        queue.offer(new int[]{nb.to, dist[nb.to]});
+                    }
                 }
             }
         }
 
-        return -1;
+        return dist[dest] == Integer.MAX_VALUE ? -1 : dist[dest];
     }
 }
