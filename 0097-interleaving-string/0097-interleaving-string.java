f61c173b24cdf06b071268a36bc7985f17d64da6
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-07-26 21:59:16 -0400

    Time: 1 ms (93.4%), Space: 44 MB (9.11%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,26 +1,63 @@
 class Solution {
+    Boolean[][] dp;
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
-        Boolean[][] dp = new Boolean[s1.length()][s2.length()];
-        return dfs(s1, 0, s2, 0, s3, 0, dp);
+        /*
+            if i == len
+                s[j:] == s[k:]
+            if j == len
+                s[i:] == s[k:]
+            if i == j
+                dfs(i+1) or dfs(j+1)
+            if i == s[k]
+                dfs(i+1)            
+            dfs(j+1)
+
+            s1 = "abdc"
+                      i  
+            s2 = "bacd"
+                      j
+            s3 = "ababcdcd"
+                        k = i + j
+                (0,0)
+                (1,0)
+            /           \
+           (2,0)        (1,1)
+        */
+        dp = new Boolean[s1.length()][s2.length()];
+        return dfs(s1, s2, 0, 0, s3);
     }
 
-    private boolean dfs(String s1, int i, String s2, int j, String s3, int k, Boolean[][] dp) {
-        if (i >= s1.length() && j >= s2.length() && k >= s3.length()) return true;
-        if (i >= s1.length()) return s2.substring(j).equals(s3.substring(k));
-        if (j >= s2.length()) return s1.substring(i).equals(s3.substring(k));
-        if (dp[i][j] != null) return dp[i][j];
-        char c1 = s1.charAt(i);
-        char c2 = s2.charAt(j);
-        char c3 = s3.charAt(k);
-        boolean currResult = false;
-        if (c1 == c3 && c2 == c3) {
-            currResult = dfs(s1, i+1, s2, j, s3, k+1, dp) || dfs(s1, i, s2, j+1, s3, k+1, dp);
-        } else if (c1 == c3) {
-            currResult = dfs(s1, i+1, s2, j, s3, k+1, dp);
-        } else if (c2 == c3) {
-            currResult = dfs(s1, i, s2, j+1, s3, k+1, dp);
+    private boolean dfs(String a, String b, int i, int j, String c) {
+        int k = i + j;
+        if (i == a.length()) {            
+            while (j < b.length()) {
+                if (b.charAt(j) != c.charAt(k))
+                    return false;
+                j++;
+                k++;
+            }
+            return true;
         }
-        return dp[i][j] = currResult;
+        if (j == b.length()) {
+            while (i < a.length()) {
+                if (a.charAt(i) != c.charAt(k))
+                    return false;
+                i++;
+                k++;
+            }
+            return true;
+        }
+        if (dp[i][j] != null) return dp[i][j];
+        if (a.charAt(i) == b.charAt(j) && a.charAt(i) == c.charAt(k)) {
+            return dp[i][j] = dfs(a, b, i + 1, j, c) || dfs(a, b, i, j + 1, c);
+        }
+        if (a.charAt(i) == c.charAt(k)) {
+            return dp[i][j] = dfs(a, b, i + 1, j, c);
+        }
+        if (b.charAt(j) == c.charAt(k)) {
+            return dp[i][j] = dfs(a, b, i, j + 1, c);
+        }
+        return false;
     }
 }
