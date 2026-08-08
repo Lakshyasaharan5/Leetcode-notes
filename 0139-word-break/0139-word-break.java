3e7dbf70f555dd08bc85a3761f154710f343404f
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-08-08 19:55:26 -0400

    Time: 9 ms (24.53%), Space: 45.9 MB (63.11%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,36 @@
-class Solution {    
+class Solution {
+    Boolean[] dp;
     public boolean wordBreak(String s, List<String> wordDict) {
-        // 0 1 2 3 4 5 6 7 8
-        // c a t s a n d o g
-        HashSet<String> wordSet = new HashSet<>();
-        for (String word : wordDict) {
-            wordSet.add(word);
-        }
-        Boolean[] dp = new Boolean[s.length() + 1];
-        return dfs(s, wordSet, 0, dp);
+        /**
+                    acb
+                    a,          ac,         acb
+                   c,cb         b
+                   b
+
+               [a,ac,cb]     
+        
+         */
+        dp = new Boolean[s.length()];
+        return dfs(s, wordDict, 0);
     }
 
-    private boolean dfs(String s, Set<String> wordSet, int start, Boolean[] dp) {
+    private boolean dfs(String s, List<String> wd, int start) {
         if (start >= s.length()) return true;
         if (dp[start] != null) return dp[start];
-        for(int i = start; i < s.length(); i++) {
-            if (wordSet.contains(s.substring(start, i + 1))) {
-                if (dfs(s, wordSet, i + 1, dp)) {
-                    dp[start] = true;
-                    return true;
-                }
+        for (int i=start; i < s.length(); i++) {
+            String curr = s.substring(start,i + 1);
+            if (match(curr, wd)) {
+                if (dfs(s, wd, i + 1))
+                    return dp[start] = true;
             }
         }
-        dp[start] = false;
+        return dp[start] = false;
+    }
+
+    private boolean match(String curr, List<String> wd) {
+        for (String w : wd)
+            if (curr.equals(w))
+                return true;
         return false;
     }
 }
