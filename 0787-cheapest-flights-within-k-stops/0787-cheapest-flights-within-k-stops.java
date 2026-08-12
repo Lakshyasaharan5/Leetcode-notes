c681a631adca381d33f3d6e11c8261e84b0d4fc3
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-08-11 22:25:42 -0400

    Time: 6 ms (85.48%), Space: 46.3 MB (92.84%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,21 +1,57 @@
+class Edge {
+    int to;
+    int cost;
+
+    Edge(int to, int cost) {
+        this.to = to;
+        this.cost = cost;
+    }
+}
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] d = new int[n];
-        Arrays.fill(d, Integer.MAX_VALUE);
-        d[src] = 0;
-        while (k >= 0) {
-            int[] t = new int[n];
-            Arrays.fill(t, Integer.MAX_VALUE);
-            t[src] = 0;
-            for (int[] edge : flights) {
-                int u = edge[0], v = edge[1], cost = edge[2];
-                if (d[u] != Integer.MAX_VALUE) {
-                    t[v] = Math.min(t[v], d[u] + cost);
+        Map<Integer, List<Edge>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            graph.put(i, new ArrayList<>());
+        for (int[] e : flights) {
+            int u = e[0], v = e[1], cost = e[2];
+            graph.get(u).add(new Edge(v, cost));
+        }
+        
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        Queue<int[]> q = new LinkedList<>(); 
+        q.offer(new int[]{src, 0}); //node, cost
+        k++;
+        while (k-- > 0) {
+            int size = q.size();
+            while (size-- > 0) {                
+                int[] front = q.poll();
+                int u = front[0], cost = front[1];
+                for (Edge nb : graph.get(u)) {
+                    if (cost + nb.cost < dist[nb.to]) {
+                        dist[nb.to] = cost + nb.cost;
+                        q.offer(new int[]{nb.to, dist[nb.to]});
+                    }
                 }
             }
-            d = t;
-            k--;
-        }
-         return d[dst] == Integer.MAX_VALUE ? -1 : d[dst];
+        }  
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        /**
+                 />-------10------>-\
+                0 --7---> 1 ---1----> 3
+                 \----1--->2 ---9--->/
+                            \
+                             \---11---->4
+
+                k = 0
+                q = [,(3,8)] // [node, cost]
+                front = 
+                d = [0 7 1  8 ~]
+                     0 1 2  3 4
+         */      
     }
+
+   
 }
