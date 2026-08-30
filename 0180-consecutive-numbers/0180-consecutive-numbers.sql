0f78fda44bb649a5f6c782e131dd5636bd4e5923
Lakshya Saharan <62757512+Lakshyasaharan5@users.noreply.github.com>
2026-08-30 14:36:49 -0400

    Time: 922 ms (7.32%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -19,9 +19,7 @@
 
 -- id+1 = num
 
-with temp as (select distinct l1.num as ConsecutiveNums
+select distinct l1.num as ConsecutiveNums
 from Logs l1
 join Logs l2 on l1.num = l2.num and l1.id + 1 = l2.id
-join Logs l3 on l2.num = l3.num and l2.id + 1 = l3.id)
-select *
-from temp;
+join Logs l3 on l2.num = l3.num and l2.id + 1 = l3.id;
